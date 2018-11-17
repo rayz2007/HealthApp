08639fd53d9383cf946c32b08c5ce9c3c384ec04
rayz2007 <rayz2007@uw.edu>
2018-11-17 01:41:40 -0800

    Updates

---
+++ precache-manifest.2524dd9a204312334315881061b38944.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/HealthApp/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "5ba70a07a668d3759437",
+    "url": "/HealthApp/static/js/runtime~main.5ba70a07.js"
+  },
+  {
+    "revision": "d5ded734e1ad5d8098d0",
+    "url": "/HealthApp/static/js/main.d5ded734.chunk.js"
+  },
+  {
+    "revision": "578efa77c66a757955ba",
+    "url": "/HealthApp/static/js/1.578efa77.chunk.js"
+  },
+  {
+    "revision": "d5ded734e1ad5d8098d0",
+    "url": "/HealthApp/static/css/main.7ce65dbc.chunk.css"
+  },
+  {
+    "revision": "578efa77c66a757955ba",
+    "url": "/HealthApp/static/css/1.6a641add.chunk.css"
+  },
+  {
+    "revision": "34332f7934c2f6b21136c8e45c23a585",
+    "url": "/HealthApp/index.html"
+  }
+];
